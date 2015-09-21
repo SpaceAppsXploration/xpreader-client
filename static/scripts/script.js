49f76ed9676b470a3410760a2940ad0dc15681d8
Luis Yoshida <luis.yoshida@gmail.com>
2015-09-21 00:01:10 -0300

    listing of articles

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -4,14 +4,32 @@ var Article = Backbone.Model.extend({
 
 });
 
-var ArticleList = new Backbone.Collection.extend({
+var Articles = Backbone.Collection.extend({
   model: Article,
+  url: 'http://hypermedia.projectchronos.eu/articles/?api=true',
 
   parse: function(response) {
-    return response.results
-  },
-
-  getArticles: function() {
-
+    return response.articles;
   }
 });
+
+var articles = new Articles();
+
+
+
+articles.fetch({
+  success: function() {
+
+    // Compiles the template
+    var articleTemplateContent= $('#article-template').text();
+    var articleTemplate = Handlebars.compile(articleTemplateContent);
+
+    articles.forEach(function(article) {
+      console.log(articleTemplate(article.attributes));
+      $('.articles').append(articleTemplate(article.attributes))
+    })
+  },
+  error: function() {
+    console.log('error')
+  }
+})
