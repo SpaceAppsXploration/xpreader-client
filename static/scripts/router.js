4a16a7875c32b838c1c5182d73b50a89c1937060
Luis Yoshida <luis.yoshida@gmail.com>
2015-09-23 22:45:43 -0300

    'Working on partial templates.

--- static/scripts/router.js
+++ static/scripts/router.js
@@ -17,9 +17,10 @@ define(['backbone', 'views', 'collections'], function(Backbone, views, collectio
     },
 
     articles: function() {
-      $('.main-content').empty();
-      $('.main-content').append('<h1>Articles</h1>');
-      $('.main-content').append('<div class="articles"><div class="throbber-loader loader hidden"></div></div>');
+      $('.main-content')
+        .empty()
+        .html(new views.ArticleView().render().el);
+
       $('.loader').removeClass('hidden');
       var articles = new collections.Articles();
       // async call to JSON API
@@ -29,14 +30,13 @@ define(['backbone', 'views', 'collections'], function(Backbone, views, collectio
             // hides the loader element
             $('.loader').addClass('hidden');
             // instantiate the big div for articles passing in the collection
-            $('.articles').html(new views.ArticleListView({ collection: articles }).render().el);
+            $('.articles h1').after(new views.ArticleListView({ collection: articles }).render().el);
           },
           error: function() {
             console.log('Error fetching articles.');
           }
         }
       );
-
     },
 
     about: function() {
@@ -50,6 +50,6 @@ define(['backbone', 'views', 'collections'], function(Backbone, views, collectio
   // works like module.exports in Nodejs
   return {
     ArticlesRouter: ArticlesRouter
-  }
+  };
 
 });
